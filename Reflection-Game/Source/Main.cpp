d60d988145c176d99a34e4a9f1c61dbc7fc78710
Anne Wijning <amwijning@hotmail.com>
2018-02-13 13:30:08 +0100

    Initial Commit. Added project skeleton.

---
+++ Reflection-Game/Source/Main.cpp
@@ -0,0 +1,20 @@
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE instance,
+		HINSTANCE previous,
+		LPSTR arguments,
+		int code) {
+
+	// Window and engine init
+	bool isRunning = true;
+
+	// Message que and game loop
+	while (isRunning){
+		// PostMessage
+		// DispatchMessage
+		// GetMessage
+		// PeekMessage
+	}
+
+	return 0;
+}
